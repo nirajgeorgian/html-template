2484d1973bb9f986c5a03d44b2b414faf0904a12
shivamsinghps <shivamsinghps@gmail.com>
2021-01-21 20:28:32 +0530

    done

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require('dotenv-safe').config();
+require('dotenv-safe').config({
+  allowEmptyValues:true
+});
 
 const nodemailer = require('nodemailer');
 const Email = require('email-templates');
